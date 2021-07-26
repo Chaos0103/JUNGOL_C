c36cb4d5ef8646224993a00694ed5e3dcba62321
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-26 17:55:50 +0900

    Create 자가진단_09.c

---
+++ Language_Coder/입력/자가진단_09.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+	double var1, var2, var3;
+
+	scanf("%lf", &var1);
+	scanf("%lf", &var2);
+	scanf("%lf", &var3);
+
+	printf("%.3lf\n", var1);	
+	printf("%.3lf\n", var2);
+	printf("%.3lf\n", var3);
+
+	return 0;
+}
