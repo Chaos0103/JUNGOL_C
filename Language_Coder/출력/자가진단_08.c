e0cfda44c92a21dcee15996d0ab28d445e3d763f
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-26 17:19:18 +0900

    Create 자가진단_08.c

---
+++ Language_Coder/출력/자가진단_08.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main()
+{
+	printf("      item     count     price\n");
+	printf("       pen        20       100\n");
+	printf("      note         5        95\n");
+	printf("    eraser       110        97\n");
+	return 0;
+}
