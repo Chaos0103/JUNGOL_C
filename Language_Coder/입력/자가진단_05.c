8956840dafa3036da28065107f4deffdf0371647
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-26 17:32:32 +0900

    Create 자가진단_05.c

---
+++ Language_Coder/입력/자가진단_05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main()
+{
+	float yard, inch;
+	
+	yard = 91.44;
+	inch = 2.54;
+
+	printf("%4.1fyd = %5.1fcm\n", 2.1, 2.1 * yard);
+	printf("%4.1fin = %5.1fcm\n", 10.5, 10.5 * inch);
+
+	return 0;
+}
