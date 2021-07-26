7b53de2c5948a8077bac849810bd620c0e60e4d5
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-27 00:33:40 +0900

    Create 자가진단_09.c

---
+++ Language_Coder/선택제어문/자가진단_09.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3;
+	int min;
+
+	scanf("%d %d %d", &num1, &num2, &num3);
+
+	min = (num1 < num2) ? num1 : num2;
+	min = (min < num3) ? min : num3;
+
+	printf("%d\n", min);
+
+	return 0;
+}
