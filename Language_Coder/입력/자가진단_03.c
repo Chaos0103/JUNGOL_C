86133665b60c19d922107b413fe007077812f6a0
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-26 17:27:19 +0900

    Create 자가진단_03.c

---
+++ Language_Coder/입력/자가진단_03.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int num1, num2;
+	
+	num1 = 55;
+	num2 = 10;
+
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+
+	num1 = 2008;
+	num2 = 1999;
+
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+
+	return 0;
+}
