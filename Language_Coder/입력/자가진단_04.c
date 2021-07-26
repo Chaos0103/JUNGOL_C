f79243a30beec18f9eb28b44817965da9c7c2f4a
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-26 17:29:28 +0900

    Create 자가진단_04.c

---
+++ Language_Coder/입력/자가진단_04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int weight;
+	float gravity;
+	
+	weight = 49;
+	gravity = 0.2683;
+	
+	printf("%d * %f = %f\n", weight, gravity, weight * gravity);
+
+	return 0;
+}
