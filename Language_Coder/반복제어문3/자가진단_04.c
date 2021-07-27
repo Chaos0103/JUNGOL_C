7cf9a12d8bc723ae3c2a8db2deb69850436bbea8
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-27 23:43:30 +0900

    Create 자가진단_04.c

---
+++ Language_Coder/반복제어문3/자가진단_04.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int num;
+
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf(" ");
+		}
+		for (int j = i; j < num; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
