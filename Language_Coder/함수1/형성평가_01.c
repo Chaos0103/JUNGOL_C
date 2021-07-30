f64751dfd605442eb534636a238bfd616051a24d
Chaos0103 <85945540+Chaos0103@users.noreply.github.com>
2021-07-31 01:17:23 +0900

    Create 형성평가_01.c

---
+++ Language_Coder/함수1/형성평가_01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void Print()
+{
+	printf("@@@@@@@@@@\n");
+}
+int main()
+{
+	printf("first\n");
+	Print();
+	printf("second\n");
+	Print();
+	printf("third\n");
+	Print();
+
+	return 0;
+}
